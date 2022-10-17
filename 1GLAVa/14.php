c0a55570d0b6b0f305fd1ca7b09d592137f5f445
kinier <govormaxsim@gmail.com>
2022-10-17 12:32:58 +0300

    task

---
+++ 1GLAVa/14.php
@@ -0,0 +1,27 @@
+<?php
+
+$pairedSimpleNumbers = 0;
+$pred = null;
+
+for ($i = 1; $i<1000; $i++){
+    $howMuchDivide = 0;
+
+    $number = $i;
+    for ($b = 1; $b<=$number; $b++){
+        if ($number%$b === 0)
+            $howMuchDivide++;
+    }
+
+    if ($howMuchDivide <= 2){
+        if ($pred === null){
+            $pred = $number;
+            continue;
+        }
+
+        if ($number - $pred === 2){
+            echo "Оно $number - $pred" . PHP_EOL;
+        }
+
+        $pred = $number;
+    }
+}
