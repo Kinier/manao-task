82bb2ad78d54b2d2cbc684b618b8adba29234b53
kinier <govormaxsim@gmail.com>
2022-10-18 22:00:09 +0300

    task

---
+++ 3GLAVa/5.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$n = 4;
+$m = 5;
+$p = 10;
+$array = [
+    [10, 4, 3, 40],
+    [10, 1000, 10000, 500],
+    [10, 5, 6, 1000],
+    [10, 20, 30, 150],
+    [10, 35, 340, 50],
+
+];
+
+
+$sum = 0;
+$sumAll = 0;
+for ($i = 0; $i < $n; $i++) {
+    $isStolbecOkay = true;
+    $sum = 0;
+    for ($j = 0; $j<$m; $j++){
+        if ($array[$j][$i]%$p !== 0){
+            $isStolbecOkay = false;
+        }else{
+            $sum+=$array[$j][$i];
+        }
+    }
+
+    if ($isStolbecOkay === true){
+        $sumAll+=$sum;
+    }
+
+    echo PHP_EOL;
+}
+echo $sumAll;
+
